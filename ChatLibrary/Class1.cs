606c9f496191dd93856db6ea96ae4a264449e04f
ipz222_tmo <ipz222_tmo@student.ztu.edu.ua>
2023-12-18 00:01:26 +0200

    Лабораторна робота №4.

---
+++ ChatLibrary/Class1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+
+namespace ChatLibrary
+{
+    [ServiceContract(CallbackContract = typeof(IChatServiceCallback))]
+    public interface IChatService
+    {
+        [OperationContract]
+        int Connect(string username);
+        [OperationContract(IsOneWay = true)]
+        void Disconnect(int id);
+        [OperationContract(IsOneWay = true)]
+        void SendMessage(string message, int id);
+        [OperationContract]
+        List<string> GetOnlineUsers();
+    }
+
+    [ServiceContract]
+    public interface IChatServiceCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void SendMessageToClient(string message);
+    }
+
+    public class ChatUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public OperationContext Context { get; set; }
+    }
+
+
+
+
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class ChatService : IChatService
+    {
+        List<ChatUser> usersList = new List<ChatUser>();
+        int nextUserId = 1;
+
+        public int Connect(string username)
+        {
+            ChatUser user = new ChatUser()
+            {
+                Id = nextUserId++,
+                Name = username,
+                Context = OperationContext.Current,
+            };
+            SendMessage(": " + user.Name + " доєднався до чату!", 0);
+            usersList.Add(user);
+            return user.Id;
+        }
+
+        public void Disconnect(int id)
+        {
+            var user = usersList.FirstOrDefault(x => x.Id == id);
+            if (user != null)
+            {
+                usersList.Remove(user);
+                SendMessage(": " + user.Name + " покинув чат!", 0);
+            }
+        }
+
+        public void SendMessage(string message, int id)
+        {
+            foreach (ChatUser item in usersList)
+            {
+                string answer = DateTime.Now.ToShortTimeString();
+
+                var user = usersList.FirstOrDefault(x => x.Id == id);
+                if (user != null)
+                {
+                    answer += ": " + user.Name + ":: ";
+                }
+                answer += message;
+
+                item.Context.GetCallbackChannel<IChatServiceCallback>().SendMessageToClient(answer);
+            }
+        }
+
+        public List<string> GetOnlineUsers()
+        {
+            return usersList.Select(u => u.Name).ToList();
+        }
+    }
+}
---
+++ Client2/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using Client2.ServiceChat2;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using ChatLibrary;
+
+namespace Client2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, Client2.ServiceChat2.IChatServiceCallback
+    {
+        bool IsConnected = false;
+        ServiceChat2.ChatServiceClient client;
+        int ID;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        void ConnectUser()
+        {
+            if (!IsConnected)
+            {
+                client = new ChatServiceClient(new System.ServiceModel.InstanceContext(this));
+                ID = client.Connect(tbUserName.Text);
+                tbUserName.IsEnabled = false;
+                bConnectDiscon.Content = "Disconect";
+                IsConnected = true;
+
+                UpdateOnlineUsers();
+            }
+        }
+
+        void DisconectUser()
+        {
+            if (IsConnected)
+            {
+                client.Disconnect(ID);
+                client = null;
+
+                tbUserName.IsEnabled = true;
+                bConnectDiscon.Content = "Connect";
+                IsConnected = false;
+
+                lbUsersOnline.Items.Clear();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsConnected)
+            {
+                DisconectUser();
+            }
+            else
+            {
+                ConnectUser();
+            }
+        }
+
+        public void SendMessageToClient(string message)
+        {
+            lbChatWindow.Items.Add(message);
+            lbChatWindow.ScrollIntoView(lbChatWindow.Items[lbChatWindow.Items.Count - 1]);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DisconectUser();
+        }
+
+        private void tbMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (client != null)
+                {
+                    client.SendMessage(tbMessage.Text, ID);
+                    tbMessage.Text = string.Empty;
+                }
+            }
+
+        }
+
+        public void UpdateOnlineUsers()
+        {
+            lbUsersOnline.Items.Clear();
+            var onlineUsers = client.GetOnlineUsers();
+            foreach (var user in onlineUsers)
+            {
+                lbUsersOnline.Items.Add(user);
+            }
+        }
+    }
+}
